5042690f6291651f4e1de5f04e10003445dec2ea
Yujeong Choung <yjchoung10@gmail.com>
2021-03-07 23:40:16 -0800

    webpack changed

--- webpack.config.js
+++ webpack.config.js
@@ -20,12 +20,9 @@ module.exports = {
       },
       {
         test: /\.js$|jsx/,
-        exclude: /(node_modules)|(build)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
         },
       },
       {
