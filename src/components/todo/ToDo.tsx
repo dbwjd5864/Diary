20ffaffaf838dc79ec0ee0ce42fff786116d7ed4
Yujeong Choung <yujeong.choung@gmail.com>
2022-09-28 23:01:03 +0900

    fix: ToDo 컴포넌트 변경

--- src/components/todo/ToDo.tsx
+++ src/components/todo/ToDo.tsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import { MdCheckBoxOutlineBlank } from 'react-icons/md';
 
-type ToDoProps = {
-  lists: Array<any>
+interface ToDoProps {
+  lists: any[];
 }
 
-const ToDo = ({lists} : ToDoProps) => {
-
-    return (
-      <>
-        <div className="listBox">
-          <fieldset>
-            <legend>To Do List</legend>
-            <ul className="toDo-list">
-              {lists.map(list => (
+const ToDo = ({ lists }: ToDoProps) => {
+  return (
+    <>
+      <div className="listBox">
+        <fieldset>
+          <legend>To Do List</legend>
+          <ul className="toDo-list">
+            {lists.map(list => {
+              return (
                 <li key={list}>
                   <MdCheckBoxOutlineBlank aria-hidden="true" />
                   {list}
                 </li>
-              ))}
-            </ul>
-          </fieldset>
-        </div>
-      </>
-  )
-}
+              );
+            })}
+          </ul>
+        </fieldset>
+      </div>
+    </>
+  );
+};
 export default ToDo;
