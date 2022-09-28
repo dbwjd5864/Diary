1608cda3441bec5e8367e5b0c1bb7b50d82b2a1a
Yujeong Choung <yujeong.choung@gmail.com>
2022-09-28 22:58:32 +0900

    feat: root 컴포넌트 아이디 변경

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './style.css';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import { GlobalStyles } from './styles/globalStyles';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('react-container') as HTMLElement,
+  document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
