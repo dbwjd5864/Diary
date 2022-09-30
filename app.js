ca7dac9ed14c42c9f2a23f64ea94c46a47bdcc47
Yujeong Choung <yujeong.choung@gmail.com>
2022-09-30 22:53:03 +0900

    fix: 새로고침시 페이지가 보이도록 수정

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const app = express();
 const connection = require('./db/connection.js');
 
+const router = require('./routes/index.js');
+const path = require('path');
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('build'));
@@ -11,10 +14,16 @@ app.use(express.json());
 connection.once('open', () => {
   console.log('connected to database');
 
-  const server = app.listen(process.env.PORT || 3000, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log(`Listenning on ${process.env.PORT || 3000}`);
   });
 });
 
-const router = require('./routes/index.js');
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, './build/index.html'), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
 app.use('/api/v1', router);
