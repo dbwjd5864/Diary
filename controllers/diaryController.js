003af859011e62d378d2b4bdea12bb016e730442
Yujeong Choung <yujeong.choung@gmail.com>
2022-10-01 22:19:21 +0900

    fix: 다이어리 데이터 리턴 형식 변경

--- controllers/diaryController.js
+++ controllers/diaryController.js
@@ -1,31 +1,34 @@
 const Diary = require('../models/Diary.js');
 
-//Get the latest diary
+// Get the latest diary
 exports.getDiary = (req, res) => {
   Diary.find({})
     .sort({ _id: -1 })
     .limit(1)
     .exec()
     .then(result => {
-      res.status(200).send(result);
+      let data = '';
+      if (result[0].diary) {
+        data = result[0].diary;
+      }
+      res.status(200).send(data);
     })
-    .catch(error => {
+    .catch(() => {
       res.status(404).send('Resource not found.');
     });
 };
 
-//Post the new diary
+// Post the new diary
 exports.postDiary = (req, res) => {
-  let text = new Diary(req.body);
+  const text = new Diary(req.body);
 
   text
     .save()
     .then(result => {
       res.set('Content-location', `api/v1/diary`);
-
       res.status(201).send({
         url: `/api/v1/diary`,
-        data: result,
+        diary: result.diary,
       });
     })
     .catch(error => {
